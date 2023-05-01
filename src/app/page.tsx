f6b2ee4fe350c946e94cbb9d0276be79dd134820
Mateusz Cwojdziński <mateuszcwojdzinski@outlook.com>
2023-05-01 16:32:49 +0200

    Add prettier into the project and prettier all files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,8 @@
-import {db} from "@/lib/db";
+import { db } from '@/lib/db'
 
 const Home = async () => {
-
   await db.set('123', 'hello')
-  return (
-   <h1 className="text-red-600">Hello world</h1>
-  )
+  return <h1 className="text-red-600">Hello world</h1>
 }
 
 export default Home
