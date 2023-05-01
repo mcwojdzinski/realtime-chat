27d484598c43e63eaad14ccdf51a25dbfa0f285d
Mateusz Cwojdziński <mateuszcwojdzinski@outlook.com>
2023-05-01 16:38:24 +0200

    Run prettier for the project

--- src/app/(dashboard)/dashboard/add/page.tsx
+++ src/app/(dashboard)/dashboard/add/page.tsx
@@ -1,11 +1,13 @@
-import {FC} from "react";
-import AddFriendButton from "@/components/AddFriendButton";
+import { FC } from 'react'
+import AddFriendButton from '@/components/AddFriendButton'
 
 const Page: FC = () => {
-    return <main className="pt-8">
-        <h1 className="font-bold text-5xl mb-8">Add a friend</h1>
-        <AddFriendButton />
+  return (
+    <main className="pt-8">
+      <h1 className="font-bold text-5xl mb-8">Add a friend</h1>
+      <AddFriendButton />
     </main>
+  )
 }
 
-export default  Page
+export default Page
